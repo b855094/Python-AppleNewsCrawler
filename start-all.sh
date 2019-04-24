88d55f5cf7d671ea5939971717dbe87c109e4909
James <45479820+b855094@users.noreply.github.com>
2019-04-24 10:45:18 +0800

    Update start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-nohup sh get_news_url.sh &> getUrl.log &
-nohup sh get_news_content.sh &> getContent.log &
-nohup sh get_news_view.sh &> getView.log &
+nohup sh get_news_url.sh > getUrl.log 2>&1 &
+nohup sh get_news_content.sh > getContent.log 2>&1 &
+nohup sh get_news_view.sh > getView.log 2>&1 &
